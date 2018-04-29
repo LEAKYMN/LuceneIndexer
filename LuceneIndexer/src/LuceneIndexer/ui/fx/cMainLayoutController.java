e77567589c884308e1b8a03f166575440e78d40f
Philip M. Trenwith <ptrenwith@gmail.com>
2018-04-29 19:55:25 +0200

    Added a context menu to open file locations and files

--- LuceneIndexer/src/LuceneIndexer/ui/fx/cMainLayoutController.java
+++ LuceneIndexer/src/LuceneIndexer/ui/fx/cMainLayoutController.java
@@ -22,7 +22,9 @@ import LuceneIndexer.lucene.eDocument;
 import LuceneIndexer.drives.cDriveMediator;
 import LuceneIndexer.drives.cDrive;
 import LuceneIndexer.lucene.cIndex;
+import java.awt.Desktop;
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.text.DecimalFormat;
 import java.util.Observable;
@@ -38,15 +40,19 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Tab;
 import javafx.scene.control.cell.MapValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.control.MenuItem;
 import org.apache.commons.io.FileUtils;
 
 /**
@@ -55,32 +61,51 @@ import org.apache.commons.io.FileUtils;
  */
 public class cMainLayoutController implements Observer, Initializable
 {
-  private final DecimalFormat oNumberFormat = new DecimalFormat( "###,###,###,###" );
-  
-  @FXML private AnchorPane m_oMainAnchorPane;
-  @FXML private Label m_oStatusLabel;
-  @FXML private CheckBox m_oWholeWords;
-  @FXML private VBox m_oSearchBox;
-  @FXML private TableView m_oResultTable;
-  @FXML private Button m_oIndexButton;
-  
-  @FXML private ListView m_oDriveList;
-  
-  @FXML private Label m_oIndexDirectoryLabel;
-  @FXML private ComboBox m_oIndexDirectories;
-  @FXML private Label m_oIndexSizeLabel;
-  @FXML private Label m_oNumberOfDocumentsLabel;
-  @FXML private Button m_oDeleteIndexButton;
-  @FXML private Button m_oRefreshButton;
-  @FXML private TableView m_oTotDocsTable;
-  
-  @FXML private Tab m_oSearchTab;
-  @FXML private Tab m_oDrives;
-  @FXML private Tab m_oIndex;
 
-  private LuceneIndexer.ui.fx.cSearchTable m_oSearchTable;
+  private final DecimalFormat oNumberFormat = new DecimalFormat("###,###,###,###");
+  private ContextMenu m_oSearchResultsContextMenu = new ContextMenu();
+
+  @FXML
+  private AnchorPane m_oMainAnchorPane;
+  @FXML
+  private Label m_oStatusLabel;
+  @FXML
+  private CheckBox m_oWholeWords;
+  @FXML
+  private VBox m_oSearchBox;
+  @FXML
+  private TableView m_oResultTable;
+  @FXML
+  private Button m_oIndexButton;
+
+  @FXML
+  private ListView m_oDriveList;
+
+  @FXML
+  private Label m_oIndexDirectoryLabel;
+  @FXML
+  private ComboBox m_oIndexDirectories;
+  @FXML
+  private Label m_oIndexSizeLabel;
+  @FXML
+  private Label m_oNumberOfDocumentsLabel;
+  @FXML
+  private Button m_oDeleteIndexButton;
+  @FXML
+  private Button m_oRefreshButton;
+  @FXML
+  private TableView m_oTotDocsTable;
+
+  @FXML
+  private Tab m_oSearchTab;
+  @FXML
+  private Tab m_oDrives;
+  @FXML
+  private Tab m_oIndex;
+
+  private cSearchTable m_oSearchTable;
   private cDriveMediator oMediator;
-  
+
   private TableColumn[] lsResultHeader;
 
   @Override
@@ -112,7 +137,7 @@ public class cMainLayoutController implements Observer, Initializable
         new TableColumn(eDocument.TAG_Size)
       };
     }
-    
+
     m_oIndexButton.setId("index");
     double dTotalWidth = m_oMainAnchorPane.getPrefWidth() - 10;
     double dColumnWidth = dTotalWidth / lsResultHeader.length;
@@ -131,16 +156,79 @@ public class cMainLayoutController implements Observer, Initializable
     m_oSearchTable = new cSearchTable(dColumnWidth);
     TableView oTable = m_oSearchTable.getTable();
     m_oSearchBox.getChildren().addAll(oTable);
-    
+
+    MenuItem oOpenLocation = new MenuItem("Open File Location");
+    oOpenLocation.setOnAction(new EventHandler<ActionEvent>()
+    {
+      @Override
+      public void handle(ActionEvent event)
+      {
+        try
+        {
+          HashMap oItems = (HashMap) m_oResultTable.getSelectionModel().getSelectedItem();
+          String sPath = (String) oItems.get("Path");
+          Desktop.getDesktop().open(new File(sPath));
+          System.out.println();
+        }
+        catch (IOException ex)
+        {
+          Logger.getLogger(cMainLayoutController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+      }
+    });
+    MenuItem oPlayFile = new MenuItem("Open/Play File");
+    oPlayFile.setOnAction(new EventHandler<ActionEvent>()
+    {
+      @Override
+      public void handle(ActionEvent event)
+      {
+        String sAbsolutePath = "";
+        try
+        {
+          HashMap oItems = (HashMap) m_oResultTable.getSelectionModel().getSelectedItem();
+
+          String sPath = (String) oItems.get("Path");
+          String sFilename = (String) oItems.get("Filename");
+          sAbsolutePath = sPath + File.separator + sFilename;
+          Object sExtension = oItems.get("Extension");
+          if (sExtension != null && !(sExtension + "").isEmpty())
+          {
+            sAbsolutePath += "." + sExtension;
+          }
+          Desktop.getDesktop().open(new File(sAbsolutePath));
+          System.out.println();
+        }
+        catch (IOException ex)
+        {
+//          if (ex.getMessage().contains("No application is associated"))
+//          {
+//            try
+//            {
+//              final String cmd = String.format("cmd.exe /C start %s", "\"" + new File(sAbsolutePath).getAbsolutePath() + "\"");
+//              Runtime.getRuntime().exec(cmd);
+//            }
+//            catch (final Throwable t)
+//            {
+//              t.printStackTrace();
+//            }
+//          }
+          Logger.getLogger(cMainLayoutController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+      }
+    });
+
+    // Add MenuItem to ContextMenu
+    m_oSearchResultsContextMenu.getItems().addAll(oOpenLocation, oPlayFile);
+    m_oResultTable.setContextMenu(m_oSearchResultsContextMenu);
     oMediator = cDriveMediator.instance();
-    
+
     displayDrives();
   }
 
   @FXML
   private void handleSearch(ActionEvent event)
   {
-    new Thread(() -> 
+    new Thread(() ->
     {
       if (m_oSearchTable != null)
       {
@@ -148,22 +236,22 @@ public class cMainLayoutController implements Observer, Initializable
       }
     }, "handleSearch").start();
   }
-  
-  @FXML 
+
+  @FXML
   private void handleIndexDrives(ActionEvent event)
   {
-    switch (m_oIndexButton.getId()) 
+    switch (m_oIndexButton.getId())
     {
       case "index":
       {
         m_oIndexButton.setText("Cancel...");
         m_oIndexButton.setId("cancel");
         cProgressPanelFx[] lsDrivePanels = cProgressPanelFx.getAll();
-        for (cProgressPanelFx oDrivePanel: lsDrivePanels)
+        for (cProgressPanelFx oDrivePanel : lsDrivePanels)
         {
           oDrivePanel.getController().markAsBusyIndexing(true);
         }
-        new Thread(() -> 
+        new Thread(() ->
         {
           oMediator.startScan();
         }, "handleIndexDrives_index").start();
@@ -174,11 +262,11 @@ public class cMainLayoutController implements Observer, Initializable
         m_oIndexButton.setText("Index Drives");
         m_oIndexButton.setId("index");
         cProgressPanelFx[] lsDrivePanels = cProgressPanelFx.getAll();
-        for (cProgressPanelFx oDrivePanel: lsDrivePanels)
+        for (cProgressPanelFx oDrivePanel : lsDrivePanels)
         {
           oDrivePanel.getController().markAsBusyIndexing(false);
         }
-        new Thread(() -> 
+        new Thread(() ->
         {
           oMediator.stopScan();
           displayDrives();
@@ -187,7 +275,7 @@ public class cMainLayoutController implements Observer, Initializable
       }
     }
   }
-  
+
   @FXML
   private void handleDeleteIndex(ActionEvent event)
   {
@@ -197,13 +285,13 @@ public class cMainLayoutController implements Observer, Initializable
     if (result == cConfirmDialog.YES)
     {
       m_oTotDocsTable.getItems().clear();
-      new Thread(() -> 
+      new Thread(() ->
       {
         oMediator.stopScan();
         cProgressPanelFx[] oPanels = cProgressPanelFx.getAll();
         if (oPanels != null)
         {
-          for (cProgressPanelFx oPanel: oPanels)
+          for (cProgressPanelFx oPanel : oPanels)
           {
             oPanel.deleteMetadata();
           }
@@ -213,7 +301,7 @@ public class cMainLayoutController implements Observer, Initializable
         String[] lsFiles = oDirectory.list();
         if (lsFiles != null)
         {
-          for (String sFile: lsFiles)
+          for (String sFile : lsFiles)
           {
             File oChildFile = new File(cConfig.instance().getIndexLocation() + File.separator + sFile);
             System.out.println("Deleting Index File: " + oChildFile.getAbsolutePath());
@@ -228,11 +316,14 @@ public class cMainLayoutController implements Observer, Initializable
       }, "handleDeleteIndex").start();
     }
   }
-  
+
   @FXML
   private void handleRefresh(ActionEvent event)
   {
-    new Thread(() -> {loadIndexMetadata();}, "handleRefresh").start();
+    new Thread(() ->
+    {
+      loadIndexMetadata();
+    }, "handleRefresh").start();
   }
 
   @Override
@@ -240,7 +331,7 @@ public class cMainLayoutController implements Observer, Initializable
   {
     if (arg != null)
     {
-      Platform.runLater(() -> 
+      Platform.runLater(() ->
       {
         m_oStatusLabel.setText("Status: " + arg);
       });
@@ -250,56 +341,56 @@ public class cMainLayoutController implements Observer, Initializable
 
   public void loadIndexMetadata()
   {
-    cDrive oDrive = cDriveMediator.instance().getDrive(m_oIndexDirectories.getValue()+"");
+    cDrive oDrive = cDriveMediator.instance().getDrive(m_oIndexDirectories.getValue() + "");
     cIndex oIndex = oDrive.getIndex();
-    
+
     File oDirectory = new File(oIndex.getIndexLocation());
     long lSize = 0;
     if (oDirectory.exists())
     {
       String[] lsFiles = oDirectory.list();
-      for (String sFile: lsFiles)
+      for (String sFile : lsFiles)
       {
         File oFile = new File(oDirectory + File.separator + sFile);
         lSize += oFile.length();
       }
     }
-    
+
     int iDocuments = oIndex.getNumberOfDocuments();
-    
+
     long flSize = lSize;
-    Platform.runLater(() -> 
+    Platform.runLater(() ->
     {
       m_oIndexDirectoryLabel.setText("Index Directory: " + oDirectory.getAbsolutePath());
       m_oIndexSizeLabel.setText("Index Size: " + FileUtils.byteCountToDisplaySize(flSize));
 
       if (iDocuments == -1)
       {
-        m_oNumberOfDocumentsLabel.setText("Number of Documents: N/A" );
+        m_oNumberOfDocumentsLabel.setText("Number of Documents: N/A");
       }
       else
       {
         m_oNumberOfDocumentsLabel.setText("Number of Documents: " + oNumberFormat.format(iDocuments));
       }
     });
-    
+
     ArrayList<eDocument> topdocs = oIndex.getTopNDocuments(50);
     setIndexTableDocuments(topdocs);
   }
-  
+
   public void displayDrives()
   {
-    Platform.runLater(() -> 
+    Platform.runLater(() ->
     {
       m_oDriveList.getItems().clear();
       cProgressPanelFx[] lsPanels = oMediator.listDrives();
-      for (cProgressPanelFx oPanel: lsPanels)
+      for (cProgressPanelFx oPanel : lsPanels)
       {
         m_oDriveList.getItems().add(oPanel.getParent());
         m_oIndexDirectories.getItems().add(oPanel.getRoot());
       }
-      
-      if (m_oIndexDirectories.getItems().size()>0)
+
+      if (m_oIndexDirectories.getItems().size() > 0)
       {
         m_oIndexDirectories.setValue(m_oIndexDirectories.getItems().get(0));
       }
@@ -308,18 +399,18 @@ public class cMainLayoutController implements Observer, Initializable
 
   public void scanComplete()
   {
-    Platform.runLater(() -> 
+    Platform.runLater(() ->
     {
       m_oIndexButton.setText("Index Drives");
       m_oIndexButton.setId("index");
       update(null, "");
     });
   }
-  
+
   public void setIndexTableDocuments(ArrayList<eDocument> lsDocuments)
   {
     m_oTotDocsTable.getItems().clear();
-    for (eDocument oDoc: lsDocuments)
+    for (eDocument oDoc : lsDocuments)
     {
       Map<String, String> oDataRow = new HashMap<>();
       oDataRow.put(eDocument.TAG_Path, oDoc.sFilePath);
@@ -331,14 +422,14 @@ public class cMainLayoutController implements Observer, Initializable
       {
         oDataRow.put(eDocument.TAG_Hash, oDoc.sFileHash);
       }
-      m_oTotDocsTable.getItems().add(oDataRow);    
+      m_oTotDocsTable.getItems().add(oDataRow);
     }
   }
 
   public void setResults(ArrayList<eDocument> lsResults)
   {
     m_oResultTable.getItems().clear();
-    for (eDocument oDoc: lsResults)
+    for (eDocument oDoc : lsResults)
     {
       Map<String, String> oDataRow = new HashMap<>();
       oDataRow.put(eDocument.TAG_Path, oDoc.sFilePath);
@@ -350,7 +441,7 @@ public class cMainLayoutController implements Observer, Initializable
       {
         oDataRow.put(eDocument.TAG_Hash, oDoc.sFileHash);
       }
-      m_oResultTable.getItems().add(oDataRow);    
+      m_oResultTable.getItems().add(oDataRow);
     }
   }
 
