215e047efbae37f58c0d5d68d7085ee88ac85d9c
Philip Trenwith <philiptrenwith@twininc.net>
2018-05-09 08:28:53 +0200

    Modified the project to use Maven

---
+++ LuceneIndexer/src/main/java/LuceneIndexer/lucene/cLuceneIndexWriter.java
@@ -0,0 +1,202 @@
+/*
+ * Copyright (C) 2018 Philip M. Trenwith
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package LuceneIndexer.lucene;
+
+import LuceneIndexer.cConfig;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.Observable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.util.CharArraySet;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.FieldType;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.store.NoLockFactory;
+import org.apache.lucene.util.BytesRef;
+
+/**
+ *
+ * @author Philip M. Trenwith
+ */
+public class cLuceneIndexWriter extends Observable
+{
+  private final Object m_oLock = new Object();
+  private IndexWriter m_oIndexWriter = null;
+  private cIndex m_oIndex;
+  
+  public cLuceneIndexWriter(cIndex oIndex)
+  {
+    m_oIndex = oIndex;
+  }
+  
+  public void open()
+  {
+    //addObserver(cDriveIndexUI.instance());
+    FSDirectory m_oIndexDirectory;
+    try 
+    {
+      Path oPath = new File(m_oIndex.getIndexLocation()).toPath();
+      m_oIndexDirectory = FSDirectory.open(oPath, NoLockFactory.INSTANCE);
+      Analyzer oDefaultAnalyzer = new StandardAnalyzer(CharArraySet.copy(Collections.emptySet()));
+
+      //PerFieldAnalyzerWrapper oMultipleLanguageAnalyzer = new PerFieldAnalyzerWrapper(oDefaultAnalyzer, m_oLanguageFields);
+      IndexWriterConfig oIndexConfig = new IndexWriterConfig(oDefaultAnalyzer);
+
+      m_oIndexWriter = new IndexWriter(m_oIndexDirectory, oIndexConfig);
+    }
+    catch (Exception ex) 
+    {
+      ex.printStackTrace();
+      Logger.getLogger(cConfig.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }
+  
+  public boolean indexFile(File oFile, String sFileHash)
+  {
+    boolean bResult = false;
+    try
+    {
+      synchronized (m_oLock)
+      {
+        if (m_oIndexWriter != null)
+        {
+          Document oDocument = new Document();
+
+          // TO Stroe fields with positioning information
+          //FieldType fieldType = new FieldType(TextField.TYPE_STORED);
+    //      fieldType.setIndexOptions(IndexOptions.DOCS_AND_FREQS_AND_POSITIONS_AND_OFFSETS );
+    //      fieldType.setStoreTermVectors(true);
+    //      fieldType.setStoreTermVectorPositions(true);
+    //      fieldType.setStoreTermVectorPayloads(true);
+    //      fieldType.setStoreTermVectorOffsets(true);
+    //      fieldType.setTokenized( true );
+    //      oDocument.add(new Field(sField, sValue, fieldType));
+
+          //oDocument.add(new Field(eDocument.TAG_ID, oFile.getAbsolutePath(), new FieldType(TextField.TYPE_STORED)));
+          oDocument.add(new Field(eDocument.TAG_ID, oFile.getAbsolutePath(), new FieldType(TextField.TYPE_STORED)));
+
+          oDocument.add(new Field(eDocument.TAG_Path, oFile.getParentFile().getAbsolutePath(), new FieldType(TextField.TYPE_STORED)));
+          oDocument.add(new Field(eDocument.TAG_Filename, FilenameUtils.getBaseName(oFile.getName()), new FieldType(TextField.TYPE_STORED)));
+          oDocument.add(new Field(eDocument.TAG_Extension, FilenameUtils.getExtension(oFile.getAbsolutePath()).toLowerCase(), new FieldType(TextField.TYPE_STORED)));
+          oDocument.add(new Field(eDocument.TAG_Category, cConfig.instance().getCategory(oFile), new FieldType(TextField.TYPE_STORED)));
+          oDocument.add(new Field(eDocument.TAG_Size, oFile.length()+"", new FieldType(TextField.TYPE_STORED)));
+
+          if (cConfig.instance().getHashDocuments())
+          {
+            oDocument.add(new Field(eDocument.TAG_Hash, sFileHash, new FieldType(TextField.TYPE_STORED)));
+          }
+
+          m_oIndexWriter.updateDocument(new Term(eDocument.TAG_ID, new BytesRef(oFile.getAbsolutePath())), oDocument);
+          bResult = true;
+        }
+      }
+    }
+    catch (IOException ex)
+    {
+      Logger.getLogger(cLuceneIndexWriter.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    return bResult;
+  }
+  
+  public boolean deleteFile(File oFile)
+  {
+    boolean bResult = false;
+    if (oFile != null && m_oIndexWriter != null)
+    {
+      try
+      {
+        synchronized (m_oLock)
+        {
+          Term term = new Term(eDocument.TAG_ID, new BytesRef(oFile.getAbsolutePath()));
+
+          System.out.println("Deleting file from index: " + term.toString());
+          m_oIndexWriter.deleteDocuments(term);
+
+          bResult = true;
+        }
+      }
+      catch (Exception ex)
+      {
+        bResult = false;
+        Logger.getLogger(cLuceneIndexWriter.class.getName()).log(Level.SEVERE, null, ex);
+      }
+    }
+    return bResult;
+  }
+  
+  public void close()
+  {
+    synchronized (m_oLock)
+    {
+      if (m_oIndexWriter != null)
+      {
+        try
+        {
+          m_oIndexWriter.commit();
+          m_oIndexWriter.close();
+        }
+        catch (IOException ex)
+        {
+          Logger.getLogger(cLuceneIndexWriter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        finally 
+        {
+          m_oIndexWriter = null;
+        }
+      }
+    }
+  }
+  
+  public void commit()
+  {
+    synchronized (m_oLock)
+    {
+      if (m_oIndexWriter != null)
+      {
+        try
+        {
+          if (m_oIndexWriter.isOpen())
+          {
+            m_oIndexWriter.commit();
+          }
+        }
+        catch (IOException ex)
+        {
+          Logger.getLogger(cLuceneIndexWriter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+      }
+    }
+  }
+  
+  public void setStatus(String sStatus)
+  {
+    setChanged();
+    notifyObservers(sStatus);
+  }
+}
---
+++ LuceneIndexer/src/main/java/LuceneIndexer/ui/fx/cProgressPanelFx.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright (C) 2018 Philip M. Trenwith
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package LuceneIndexer.ui.fx;
+
+import LuceneIndexer.drives.cDrive;
+import LuceneIndexer.drives.folderwatcher.cFolderWatcher;
+import java.io.File;
+import java.util.Collection;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Philip Trenwith
+ */
+public class cProgressPanelFx extends Application
+{
+  private static HashMap<String, cProgressPanelFx> m_oProgressPanels = new HashMap();
+  private cProgressPanelController oProgressPanelController;
+  private cFolderWatcher oFolderWatcher;
+  private Parent oRoot;
+  private Scene oScene;
+  private File oDriveRoot;
+  private cDrive m_oDrive;
+  private long m_oLastStatusChangeTimestamp = -1;
+  
+  public static cProgressPanelFx get(String sRoot, cDrive oDrive)
+  {
+    cProgressPanelFx oReturn = m_oProgressPanels.get(sRoot);
+    if (oReturn == null)
+    {
+      File oFile = new File(sRoot);
+      oReturn = new cProgressPanelFx(oFile, oDrive);
+      m_oProgressPanels.put(oFile.getAbsolutePath(), oReturn);
+    }
+    return oReturn;
+  }
+  
+  public static cProgressPanelFx[] getAll()
+  {
+    Collection<cProgressPanelFx> lsDrives = m_oProgressPanels.values();
+    cProgressPanelFx[] oReturn = new cProgressPanelFx[0];
+    if (lsDrives != null)
+    {
+      oReturn = lsDrives.toArray(oReturn);
+    }
+    return oReturn;
+  }
+  
+  public static void terminateAll()
+  {
+    Collection<cProgressPanelFx> lsPanels = m_oProgressPanels.values();
+    Iterator<cProgressPanelFx> oIterator = lsPanels.iterator();
+    while (oIterator.hasNext())
+    {
+      cProgressPanelFx oPanel = oIterator.next();
+      oPanel.terminate();
+    }
+  }
+  
+  public void terminate()
+  {
+    if (oFolderWatcher != null)
+    {
+      oFolderWatcher.terminate();
+    }
+  }
+  
+  private cProgressPanelFx(File oRoot, cDrive oDrive)
+  {
+    try
+    {
+      m_oDrive = oDrive;
+      oDriveRoot = oRoot;
+      m_oProgressPanels.put(oDriveRoot.getAbsolutePath(), this);
+      start(null);
+    }
+    catch (Exception ex)
+    {
+      Logger.getLogger(cProgressPanelFx.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }
+  
+  @Override
+  public void start(Stage stage) throws Exception
+  {
+    FXMLLoader oLoader = new FXMLLoader(getClass().getResource("/fxml/cProgressPanel.fxml"));
+    oRoot = oLoader.load();
+    oScene = new Scene(oRoot);
+       
+    oProgressPanelController = oLoader.<cProgressPanelController>getController();
+    oProgressPanelController.postInitialize(oDriveRoot, m_oDrive);
+  }
+  
+  public Parent getParent()
+  {
+    return oRoot;
+  }
+  
+  public String getRoot()
+  {
+    return oDriveRoot.getPath();
+  }
+  
+  public cProgressPanelController getController()
+  {
+    return oProgressPanelController;
+  }
+
+  public void deleteMetadata()
+  {
+    oProgressPanelController.deleteMetadata();
+  }
+
+  public void resetProgress()
+  {
+    oProgressPanelController.resetProgress();
+  }
+
+  public void setStatus(String sStatus)
+  {
+    m_oLastStatusChangeTimestamp = new GregorianCalendar().getTimeInMillis();
+    oProgressPanelController.setStatus(sStatus);
+  }
+
+  public void cancel()
+  {
+    oProgressPanelController.cancel();
+  }
+
+  public void complete()
+  {
+    oProgressPanelController.complete();
+  }
+
+  public void appendIndexSize(String sFile, long length)
+  {
+    oProgressPanelController.appendIndexSize(sFile, length);
+  }
+
+  public long getLastStatusUpdateTime()
+  { 
+    if (m_oLastStatusChangeTimestamp == -1)
+    {
+      m_oLastStatusChangeTimestamp = new GregorianCalendar().getTimeInMillis();
+    }
+    return m_oLastStatusChangeTimestamp;
+  }
+}
